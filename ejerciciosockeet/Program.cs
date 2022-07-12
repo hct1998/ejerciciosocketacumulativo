ab0edfeed146f5bff96e5dcd97e680e7371a1bca
HECTOR JAVIER LECAROS GONZALEZ <hector.lecaros@inacapmail.cl>
2022-07-12 17:25:28 -0400

    prueba 1

--- ejerciciosockeet/Program.cs
+++ ejerciciosockeet/Program.cs
@@ -10,6 +10,25 @@ namespace ejerciciosockeet
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Ingrese nombre: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese Edad: ");
+            string edadtx = Console.ReadLine().Trim();
+            int edad = -1;
+            bool esValido = Int32.TryParse(edadtx, out edad);
+
+            if (!esValido) //if (!esValido) es decir If (esvalido == true) 
+            {
+                Console.WriteLine("Ingrese bien la edad");
+            }
+            else
+            {
+                Console.WriteLine("Su nombre es {0} y su edad es {1}", nombre, edad);
+            }
+
+            Console.ReadKey();
         }
     }
 }
